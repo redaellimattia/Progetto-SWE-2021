293ba6ec22457ac086a775ce7ce2ef0a49afb95c
LucaRondini <luca.rondini@mail.polimi.it>
2021-03-24 23:39:13 +0100

    Added methods to MarketDashboard.

--- src/main/java/it/polimi/ingsw/model/MarketDashboard.java
+++ src/main/java/it/polimi/ingsw/model/MarketDashboard.java
@@ -8,11 +8,42 @@ public class MarketDashboard {
         return freeMarble;
     }
 
-    public void swapStructure(){
+    public void fixGrid(){
 
     }
 
-    public void fixGrid(){
+    // Returns a copy of the selected row
+    public MarketMarble[] getRow(int row) throws IllegalArgumentException {
+        MarketMarble[] selectedRow = new MarketMarble[4];
+        if(row < 1 || row > 3) {
+            throw new IllegalArgumentException();
+        }
+        int cur;
+        for(cur = 0; cur < 4; cur++) {
+            selectedRow[cur] = structure[row-1][cur];
+        }
+        return selectedRow;
+    }
+
+    // Returns a copy of the selected column
+    public MarketMarble[] getColumn(int col) throws IllegalArgumentException {
+        MarketMarble[] selectedCol = new MarketMarble[3];
+        if(col < 1 || col > 4) {
+            throw new IllegalArgumentException();
+        }
+        int cur;
+        for(cur = 0; cur < 3; cur++) {
+            selectedCol[cur] = structure[cur][col-1];
+        }
+        return selectedCol;
+    }
+
+    // TO-DO: Return a copy of the structure instead of the structure itself
+    public MarketMarble[][] getStructure() {
+        return structure;
+    }
+
+    public void swapStructure(){
 
     }
 
