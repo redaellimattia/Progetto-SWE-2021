822271b99debbefc50689e20ea3edf3166f90067
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-03-27 18:55:22 +0100

    fixed Prod

--- src/main/java/it/polimi/ingsw/model/Production.java
+++ src/main/java/it/polimi/ingsw/model/Production.java
@@ -19,7 +19,7 @@ public class Production {
 
     public boolean isPossible(ResourceCount temp){ //True if production is possible
         //count is now equals to all the resources available to the player
-        if(input.getCoins()<=temp.getCoins()&&temp.getRocks()<=temp.getRocks()&&temp.getServants()<=temp.getServants()&&temp.getShields()<=temp.getShields())
+        if(input.getCoins()<=temp.getCoins()&&input.getRocks()<=temp.getRocks()&&input.getServants()<=temp.getServants()&&input.getShields()<=temp.getShields())
             return true; //Va decrement count
         else
             return false;
