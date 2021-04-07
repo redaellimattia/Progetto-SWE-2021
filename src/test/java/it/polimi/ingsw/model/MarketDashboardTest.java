ec1ff8f8f508f6c424baa47af2b291d8d8ca38ce
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-07 17:24:34 +0200

    Added AdvanceTokenTest and MarketDashboardTest classes

---
+++ src/test/java/it/polimi/ingsw/model/MarketDashboardTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.enumeration.MarbleColour;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketDashboardTest {
+
+    MarketDashboard buildDashboard() {
+        MarketMarble[][] testStructure = new MarketMarble[3][4];
+        testStructure[0][0] = new MarketMarble(MarbleColour.PURPLE);
+        testStructure[0][1] = new MarketMarble(MarbleColour.BLUE);
+        testStructure[0][2] = new MarketMarble(MarbleColour.PURPLE);
+        testStructure[0][3] = new MarketMarble(MarbleColour.GREY);
+        testStructure[1][0] = new MarketMarble(MarbleColour.WHITE);
+        testStructure[1][1] = new MarketMarble(MarbleColour.RED);
+        testStructure[1][2] = new MarketMarble(MarbleColour.WHITE);
+        testStructure[1][3] = new MarketMarble(MarbleColour.WHITE);
+        testStructure[2][0] = new MarketMarble(MarbleColour.WHITE);
+        testStructure[2][1] = new MarketMarble(MarbleColour.GREY);
+        testStructure[2][2] = new MarketMarble(MarbleColour.YELLOW);
+        testStructure[2][3] = new MarketMarble(MarbleColour.YELLOW);
+        MarketMarble testFreeMarble = new MarketMarble(MarbleColour.BLUE);
+        MarketDashboard testDashboard = new MarketDashboard(testStructure, testFreeMarble);
+        return testDashboard;
+    }
+
+    @Test
+    void getRow() {
+        MarketDashboard testDashboard = buildDashboard();
+        MarketMarble[] testRow = new MarketMarble[4];
+        testRow[0] = new MarketMarble(MarbleColour.PURPLE);
+        testRow[1] = new MarketMarble(MarbleColour.BLUE);
+        testRow[2] = new MarketMarble(MarbleColour.PURPLE);
+        testRow[3] = new MarketMarble(MarbleColour.GREY);
+        testDashboard.getRow(0);
+        assertEquals(testDashboard.getRow(0), testRow);
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/token/AdvanceTokenTest.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.token;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvanceTokenTest {
+
+}
