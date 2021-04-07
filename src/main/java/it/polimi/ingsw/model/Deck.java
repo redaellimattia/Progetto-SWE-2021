f75e67082c84d4a471269363c14fa5cdc9549116
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-07 18:31:56 +0200

    Deck is now an abstract class (not an interface)

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -4,10 +4,28 @@ import it.polimi.ingsw.model.card.DevelopmentCard;
 
 import java.util.ArrayList;
 
-public interface Deck {
-    public ArrayList<DevelopmentCard> getDeck();
-    public DevelopmentCard getFirst();
-    public void removeFirst();
-    public DevelopmentCard getCard(int pos);
-    public void addCard(DevelopmentCard card);
+public abstract class Deck {
+    private ArrayList<DevelopmentCard> cardsDeck;
+
+    //Return copy of cardsDeck
+    public ArrayList<DevelopmentCard> getDeck() {
+        return new ArrayList<DevelopmentCard>(cardsDeck);
+    }
+
+    public DevelopmentCard getFirst() throws IndexOutOfBoundsException {
+        return cardsDeck.get(0);
+    }
+
+    public void removeFirst() throws IndexOutOfBoundsException {
+        cardsDeck.remove(0);
+    }
+
+    public DevelopmentCard getCard(int pos) throws IndexOutOfBoundsException {
+        return cardsDeck.get(pos);
+    }
+
+    // Insert card on top
+    public void addCard(DevelopmentCard card) {
+        cardsDeck.add(0, card);
+    }
 }
--- src/main/java/it/polimi/ingsw/model/DeckShop.java
+++ src/main/java/it/polimi/ingsw/model/DeckShop.java
@@ -2,34 +2,11 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.card.DevelopmentCard;
 
-import java.util.ArrayList;
-
-public class DeckShop implements Deck{
-    private ArrayList<DevelopmentCard> cardsDeck = new ArrayList<DevelopmentCard>();
-
-    @Override
-    public ArrayList<DevelopmentCard> getDeck() {
-        return new ArrayList<DevelopmentCard>(cardsDeck);
-    } //Return copy of cardsDeck
-
-    @Override
-    public DevelopmentCard getFirst() throws IndexOutOfBoundsException {
-        return cardsDeck.get(0);
-    }
-
-    @Override
-    public void removeFirst() throws IndexOutOfBoundsException {
-        cardsDeck.remove(0);
-    }
-
-    @Override
-    public DevelopmentCard getCard(int pos) throws IndexOutOfBoundsException {
-        return cardsDeck.get(pos);
-    }
+public class DeckShop extends Deck {
 
     // Insert card on top
     @Override
-    public void addCard(DevelopmentCard card) {
-        cardsDeck.add(0, card);
+    public void addCard(DevelopmentCard card) { // TO-DO: Check if the deck is full
+        super.addCard(card);
     }
 }
