6073ee4c8d35d7e84f67799ba9cf0b2651f5c117
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-07 18:00:54 +0200

    Testing

--- src/test/java/it/polimi/ingsw/model/card/DiscountAbilityTest.java
+++ src/test/java/it/polimi/ingsw/model/card/DiscountAbilityTest.java
@@ -14,23 +14,26 @@ class DiscountAbilityTest {
         ResourceCount cost = new ResourceCount(2,2,2,2,0);
         ResourceCount expected = new ResourceCount(1,2,2,2,0);
         //COIN
-        discountAbility = new DiscountAbility(Resource.COIN);
+        discountAbility = initDiscountAbility(Resource.COIN);
         assertTrue(discountAbility.useDiscountAbility(cost)); //Returns true and decreases cost
         assertEquals(expected,cost); //Check if cost is decreased
         //ROCK
-        discountAbility = new DiscountAbility(Resource.ROCK);
+        discountAbility = initDiscountAbility(Resource.ROCK);
         expected.removeRocks(1);
         assertTrue(discountAbility.useDiscountAbility(cost)); //Returns true and decreases cost
         assertEquals(expected,cost); //Check if cost is decreased
         //SERVANT
-        discountAbility = new DiscountAbility(Resource.SERVANT);
+        discountAbility = initDiscountAbility(Resource.SERVANT);
         expected.removeServants(1);
         assertTrue(discountAbility.useDiscountAbility(cost)); //Returns true and decreases cost
         assertEquals(expected,cost); //Check if cost is decreased
         //SHIELD
-        discountAbility = new DiscountAbility(Resource.SHIELD);
+        discountAbility = initDiscountAbility(Resource.SHIELD);
         expected.removeShields(1);
         assertTrue(discountAbility.useDiscountAbility(cost)); //Returns true and decreases cost
         assertEquals(expected,cost); //Check if cost is decreased
+    }
+    DiscountAbility initDiscountAbility(Resource res){
+        return new DiscountAbility(res);
     }
 }
