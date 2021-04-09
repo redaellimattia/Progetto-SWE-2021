ccc92099ed695ff8e1991b80d9425bbce3837e78
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-09 11:03:00 +0200

    Tested TypeOfCardRequirement [OK]

--- src/test/java/it/polimi/ingsw/model/card/TypeOfCardRequirementTest.java
+++ src/test/java/it/polimi/ingsw/model/card/TypeOfCardRequirementTest.java
@@ -5,17 +5,18 @@ import it.polimi.ingsw.model.enumeration.CardColour;
 import it.polimi.ingsw.model.enumeration.Resource;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardLevelRequirementTest {
+class TypeOfCardRequirementTest {
 
     @Test
     void isPlayable() {
         PlayerDashboard player = createPlayer();
         Production prod = new Production(new ResourceCount(0,0,0,0,0),new ResourceCount(0,0,0,0,0));
-        DevelopmentCard card1 = new DevelopmentCard(1,new ResourceCount(1,0,0,0,0),prod,1,CardColour.BLUE);
+        DevelopmentCard card1 = new DevelopmentCard(1,new ResourceCount(1,0,0,0,0),prod,1, CardColour.BLUE);
         DevelopmentCard card2 = new DevelopmentCard(1,new ResourceCount(1,0,0,0,0),prod,1,CardColour.GREEN);
         DevelopmentCard card3 = new DevelopmentCard(1,new ResourceCount(1,0,0,0,0),prod,1,CardColour.YELLOW);
 
@@ -26,14 +27,14 @@ class CardLevelRequirementTest {
         player.getDevCards()[1].addCard(card2);
         player.getDevCards()[1].addCard(card3);
 
-        CardLevelRequirement req = new CardLevelRequirement(CardColour.BLUE,1);
+        TypeOfCardRequirement req = new TypeOfCardRequirement(new ColourCount(1,1,0,0)); //Playable
         assertTrue(req.isPlayable(player));
 
-        req = new CardLevelRequirement(CardColour.GREEN,1);
-        assertTrue(req.isPlayable(player));
-
-        req = new CardLevelRequirement(CardColour.YELLOW,2);
+        req = new TypeOfCardRequirement(new ColourCount(1,0,0,1)); //Not Playable
         assertFalse(req.isPlayable(player));
+
+        req = new TypeOfCardRequirement(new ColourCount(1,1,1,0)); //Playable
+        assertTrue(req.isPlayable(player));
     }
     PlayerDashboard createPlayer(){
         String nickname = "Primo";
@@ -47,7 +48,7 @@ class CardLevelRequirementTest {
         ResourceCount input = new ResourceCount(0,0,0,0,0);
         ResourceCount output = new ResourceCount(0,0,0,0,0);
         Production basicProduction = new Production(input,output);
-        ArrayList <LeaderCard> leaderCards = new ArrayList<LeaderCard>();
+        ArrayList<LeaderCard> leaderCards = new ArrayList<LeaderCard>();
         PlayerDashboard player = new PlayerDashboard(storage,chest,devCards,leaderCards,basicProduction,1,nickname,2);
         return player;
     }
