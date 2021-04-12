f62d1d3f31478e024be0dbab83160533a955c10f
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-12 15:37:20 +0200

    Updated AdvanceTokenTest

--- src/test/java/it/polimi/ingsw/model/token/AdvanceTokenTest.java
+++ src/test/java/it/polimi/ingsw/model/token/AdvanceTokenTest.java
@@ -3,14 +3,15 @@ package it.polimi.ingsw.model.token;
 import it.polimi.ingsw.model.*;
 import it.polimi.ingsw.model.card.LeaderCard;
 import it.polimi.ingsw.model.enumeration.Resource;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//TO-DO: Use Game class to build PlayerDashboard; test ReRoll effect in Game class
 class AdvanceTokenTest {
-    public void buildToken() {
+    public PlayerDashboard buildPlayerDashboard() {
         CounterTop testCounterTop1 = new CounterTop(Resource.COIN, 1);
         CounterTop testCounterTop2 = new CounterTop(Resource.ROCK, 2);
         CounterTop testCounterTop3 = new CounterTop(Resource.SHIELD, 3);
@@ -19,12 +20,24 @@ class AdvanceTokenTest {
         DeckDashboard[] testDevCards = new DeckDashboard[3];
         ArrayList<LeaderCard> testLeaderCards = new ArrayList<LeaderCard>(0);
         Production testProduction = new Production(testChest, testChest);
-        PlayerDashboard testDashboard = new PlayerDashboard(testStorage, testChest, testDevCards, testLeaderCards, testProduction, 0, "Test", 0);
+        return new PlayerDashboard(testStorage, testChest, testDevCards, testLeaderCards, testProduction, 0, "Test", 0);
+    }
+
+    @Test
+    public void testNoReRollToken() {
+        PlayerDashboard testDashboard = buildPlayerDashboard();
+        int oldPos = testDashboard.getPathPosition();
         AdvanceToken testToken = new AdvanceToken(false);
+        testToken.useToken(testDashboard);
+        assertEquals(oldPos + 2, testDashboard.getPathPosition());
     }
 
-    public void testNoRerollToken() {
-
+    @Test
+    public void testReRollToken() {
+        PlayerDashboard testDashboard = buildPlayerDashboard();
+        int oldPos = testDashboard.getPathPosition();
+        AdvanceToken testToken = new AdvanceToken(true);
+        testToken.useToken(testDashboard);
+        assertEquals(oldPos + 1, testDashboard.getPathPosition());
     }
-
 }
