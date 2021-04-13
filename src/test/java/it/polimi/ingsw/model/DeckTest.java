a00565a4367557ff5fbc493ab20573d2639e0a65
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-13 23:07:12 +0200

    Testing Deck [ONGOING]

--- src/test/java/it/polimi/ingsw/model/DeckTest.java
+++ src/test/java/it/polimi/ingsw/model/DeckTest.java
@@ -4,28 +4,34 @@ import it.polimi.ingsw.model.card.DevelopmentCard;
 import it.polimi.ingsw.model.enumeration.CardColour;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
 
-    @Test
-    ArrayList<DevelopmentCard> buildDeck() {
-        ArrayList<DevelopmentCard> testDeck = new ArrayList<DevelopmentCard>();
+    DeckDashboard buildDeck() {
+        DeckDashboard testDeck = new DeckDashboard();
         ResourceCount testResourceCount = new ResourceCount(0, 0, 0, 7, 0);
         ResourceCount testInput = new ResourceCount(0, 0, 1, 0, 0);
         ResourceCount testOutput = new ResourceCount(1, 0, 0, 0, 3);
         Production testProductionPower = new Production(testInput, testOutput);
         DevelopmentCard testCard = new DevelopmentCard(2, testResourceCount, testProductionPower, 3, CardColour.GREEN);
-        testDeck.add(testCard);
+        testDeck.addCard(testCard);
         testResourceCount = new ResourceCount(4,0,0,0,0);
         testInput = new ResourceCount(0, 0, 1, 0, 0);
         testOutput = new ResourceCount(0,0,0,0,2);
         testProductionPower = new Production(testInput, testOutput);
         testCard = new DevelopmentCard(5, testResourceCount, testProductionPower, 2, CardColour.BLUE);
-        testDeck.add(testCard);
+        testDeck.addCard(testCard);
         return testDeck;
     }
 
+    @Test
+    void testDeck() {
+        DeckDashboard testDeck = buildDeck();
+        assertEquals(4, testDeck.getCard(0).getCost().getCoins());
+        assertEquals(0, testDeck.getCard(1).getCost().getCoins());
+    }
 }
