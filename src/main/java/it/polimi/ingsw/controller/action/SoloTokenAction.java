e389c73f24929d8f99de5c84a4c9ff431a44ee73
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-17 00:16:01 +0200

    Fixing Polymorphism

--- src/main/java/it/polimi/ingsw/controller/action/SoloTokenAction.java
+++ src/main/java/it/polimi/ingsw/controller/action/SoloTokenAction.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.controller.action;
 
+import it.polimi.ingsw.controller.Parameter;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.PlayerDashboard;
 import it.polimi.ingsw.model.enumeration.CardColour;
@@ -7,38 +8,43 @@ import it.polimi.ingsw.model.token.SoloToken;
 
 public class SoloTokenAction extends Action {
 
-    public boolean drawToken(PlayerDashboard player, PlayerDashboard lorenzo) {
+    @Override
+    public boolean useAction(PlayerDashboard lorenzo, Parameter param) {
         SoloToken token = Game.pickNextToken();
         token.useToken(lorenzo);
         // TO-DO: Check if Vatican Report is triggered (if it isn't checked by model)
 
+        // Code below to be moved
+        /*
         // Check if Lorenzo has reached the end of Faith Track
-        if(lorenzo.getPathPosition() == 24) {
+        if (lorenzo.getPathPosition() == 24) {
             // Inform GameManager that user has lost
             return true;
         }
         // Check if the user has reached the end of Faith Track
-        if(player.getPathPosition() == 24) {
+        if (player.getPathPosition() == 24) {
             // Inform GameManager that user has won
             return true;
         }
         // Check if there is at least one empty column in shop (the user has lost)
         boolean emptyCol = false; // flag that indicates that there is at least one empty column
-        for(int i = 0; i < CardColour.values().length; i++) { // Check if each column has at least one non-empty deck
+        for (int i = 0; i < CardColour.values().length; i++) { // Check if each column has at least one non-empty deck
             boolean flag = false;
-            for(int j = 0; j < 2; j++) {
-                if(!Game.getShop().getGrid()[j][i].getDeck().isEmpty()) {
+            for (int j = 0; j < 2; j++) {
+                if (!Game.getShop().getGrid()[j][i].getDeck().isEmpty()) {
                     flag = true; // This column is not empty
                 }
             }
-            if(flag == false) { // At the end of each column, check if the column was empty
+            if (flag == false) { // At the end of each column, check if the column was empty
                 emptyCol = true; // At least one empty column
             }
         }
-        if(emptyCol) {
+        if (emptyCol) {
             // Inform GameManager that user has lost
             return true;
         }
+
+         */
         return true;
     }
 
