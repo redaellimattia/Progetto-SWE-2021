ac530ba76e5091d75603fa43e095c5f5826bc963
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-20 15:51:04 +0200

    ProductionAbility DONE Tested and JavaDoc DONE

--- src/main/java/it/polimi/ingsw/model/card/ProductionAbility.java
+++ src/main/java/it/polimi/ingsw/model/card/ProductionAbility.java
@@ -5,20 +5,33 @@ import it.polimi.ingsw.model.enumeration.Resource;
 
 public class ProductionAbility extends SpecialAbility {
 
+    /**
+     *
+     * @param resourceType cost for the production as Resource
+     */
     public ProductionAbility(Resource resourceType) {
         super(resourceType);
     }
 
+    /**
+     *
+     * @param count passed ResourceCount, equals to the resources chosen to pay
+     * @return true if there are enough resources to use the production
+     */
     @Override
     //WHEN IN PRODUCTIONACTION, THIS PRODUCTION IS AVAILABLE, TRUE IF POSSIBLE
     public boolean useProductionAbility(ResourceCount count){
         Resource resType = super.getResourceType();
-        if(count!=null) {
-            if (resType.get(count) == 1) //If there is exactly one res
-                return true; //True if there are enough resources to use the production
-        }
+        if(count!=null)
+            return resType.get(count) == 1; //True if there is exactly one res to use the production
         return false; //False, not enough resources
     }
+
+    /**
+     *
+     * @param o passed Object
+     * @return true if o is the same object of this, or it has the same Resource cost
+     */
     @Override
     public boolean equals(Object o){
         if (o == this) { //True if it's this instance
