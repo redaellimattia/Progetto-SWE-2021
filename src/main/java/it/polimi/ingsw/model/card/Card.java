85b7d1b85ebd462302524a32e5d2921c91888ca8
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-20 17:43:55 +0200

    LeaderCard AND DevelopmentCard DONE Tested and JavaDoc DONE

--- src/main/java/it/polimi/ingsw/model/card/Card.java
+++ src/main/java/it/polimi/ingsw/model/card/Card.java
@@ -3,10 +3,18 @@ package it.polimi.ingsw.model.card;
 public abstract class Card {
     private int victoryPoints;
 
+    /**
+     *
+     * @param victoryPoints victoryPoints value of the card
+     */
     public Card(int victoryPoints) {
         this.victoryPoints = victoryPoints;
     }
 
+    /**
+     *
+     * @return victoryPoints value
+     */
     public int getVictoryPoints() {
         return victoryPoints;
     }
