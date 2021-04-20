296d3c08deb6fdcaafdfd13c20aac88370def479
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-20 16:37:35 +0200

    SpecialAbility DONE Tested and JavaDoc DONE

--- src/main/java/it/polimi/ingsw/model/card/WhiteChangeAbility.java
+++ src/main/java/it/polimi/ingsw/model/card/WhiteChangeAbility.java
@@ -4,15 +4,28 @@ import it.polimi.ingsw.model.enumeration.Resource;
 
 public class WhiteChangeAbility extends SpecialAbility {
 
+    /**
+     *
+     * @param resourceType Resource of the whitChange
+     */
     public WhiteChangeAbility(Resource resourceType) {
         super(resourceType);
     }
 
+    /**
+     *
+     * @return Resource of the Card
+     */
     @Override
-    public Resource useWhiteChangeAbility(){ //Devo per forza ritornare, da pensare
-        return super.getResourceType();
-    } //Returns resourceType
+    public Resource useWhiteChangeAbility(){
+        return super.getResourceType(); //Returns resourceType
+    }
 
+    /**
+     *
+     * @param o passed Object
+     * @return true if o is the same object of this, or it has the same Resource
+     */
     @Override
     public boolean equals(Object o){
         if (o == this) { //True if it's this instance
