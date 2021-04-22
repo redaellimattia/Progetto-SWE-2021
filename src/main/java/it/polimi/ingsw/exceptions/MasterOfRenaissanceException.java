47b861988336217db6a28e375121de0107709355
Redaelli Mattia <redaelli.mattia10@gmail.com>
2021-04-22 11:14:07 +0200

    Added MasterOfRenaissanceException.java

---
+++ src/main/java/it/polimi/ingsw/exceptions/MasterOfRenaissanceException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class MasterOfRenaissanceException extends Exception{
+    public MasterOfRenaissanceException(String msg) {
+        super(msg);
+    }
+}
