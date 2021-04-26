5bb077a033a466287aaaefac82f174056c737003
LucaRondini <luca.rondini@mail.polimi.it>
2021-04-26 10:34:37 +0200

    JavaDoc GetResource DONE

--- src/main/java/it/polimi/ingsw/controller/action/marketAction/GetResource.java
+++ src/main/java/it/polimi/ingsw/controller/action/marketAction/GetResource.java
@@ -8,10 +8,23 @@ public class GetResource implements AtomicMarketAction {
 
     private int storageRow;
 
+    /**
+     *
+     * @param storageRow the number of the storage row in which the resource should be added
+     *                   1-3: first row, second row, third row
+     *                   4: special deposit (leader card ability)
+     */
     public GetResource(int storageRow) {
         this.storageRow = storageRow;
     }
 
+    /**
+     *
+     * @param marble the marble to convert
+     * @param player the player performing the action
+     * @return true if ended correctly
+     *         false if the action was illegal
+     */
     @Override
     public boolean useAction(MarketMarble marble, PlayerDashboard player) {
         if (marble.getColour() == MarbleColour.WHITE || marble.getColour() == MarbleColour.RED) {
