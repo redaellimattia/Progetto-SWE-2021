1dbf5ec69a97fcea5a98ee44d6e3e7bed4060450
LucaRondini <luca.rondini@mail.polimi.it>
2021-05-15 11:43:34 +0200

    Testing getFourLeaders method

--- src/test/java/it/polimi/ingsw/controller/GameLobbyTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameLobbyTest.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.card.LeaderCard;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +38,18 @@ class GameLobbyTest {
         testLobby.addPlayer("Giocatore");
         testLobby.initGame(true,null);
         GameManager gameManager  = testLobby.getGameManager();
+    }
+
+    @Test
+    void getFourLeadersTest() {
+        GameLobby testLobby = new GameLobby(0, 4);
+        testLobby.addPlayer("Pippo");
+        testLobby.addPlayer("Pluto");
+        testLobby.addPlayer("Paperino");
+        testLobby.addPlayer("Giocatore");
+        testLobby.initGame(false,null);
+        GameManager gameManager  = testLobby.getGameManager();
+        ArrayList<LeaderCard> leadersTest1 = testLobby.getFourLeaders("Pippo");
+        ArrayList<LeaderCard> leadersTest2 = testLobby.getFourLeaders("Pluto");
     }
 }
