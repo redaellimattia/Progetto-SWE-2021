7bfbae6bb8aacb438d165ca098d19629d523a61a
redaellimattia <redaelli.mattia10@gmail.com>
2021-06-01 11:19:17 +0200

    Setting gui up

--- src/main/java/it/polimi/ingsw/view/gui/GuiMain.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiMain.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class GuiMain extends Application {
 
     @Override
     public void start(Stage stage) {
