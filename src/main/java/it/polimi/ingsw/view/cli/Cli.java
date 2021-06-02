5eeee8bbc81a502cd890c7611fa273f182fa614d
LucaRondini <luca.rondini@mail.polimi.it>
2021-06-02 12:51:16 +0200

    Added JavaDoc

--- src/main/java/it/polimi/ingsw/view/cli/Cli.java
+++ src/main/java/it/polimi/ingsw/view/cli/Cli.java
@@ -631,6 +631,12 @@ public class Cli implements View {
         }while(input.equalsIgnoreCase("y"));
     }
 
+    /**
+     * Called if the user decides to take resources from market
+     * The user must type "row" or "col" and the number of the chosen row/col
+     * Then, for every marble the user will choose what to do with it or,
+     * if there is only one choice possible, the method will print what happened
+     */
     @Override
     public void takeResourcesFromMarket(){
         String input;
