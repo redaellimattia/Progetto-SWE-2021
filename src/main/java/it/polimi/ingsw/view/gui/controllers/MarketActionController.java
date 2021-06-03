4a9f8e09688f73d08cf43020de0a77b13a25086d
LucaRondini <luca.rondini@mail.polimi.it>
2021-06-03 22:54:00 +0200

    Updated MarketAction GUI

--- src/main/java/it/polimi/ingsw/view/gui/controllers/MarketActionController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/MarketActionController.java
@@ -1,6 +1,25 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import it.polimi.ingsw.network.client.ClientManager;
+import it.polimi.ingsw.view.gui.GuiManager;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class MarketActionController extends GuiController {
+
+    @FXML
+    private Pane col1, col2, col3, col4, row1, row2, row3;
+
+    private int type;
+    private int pos;
+    private ClientManager clientManager;
+
+    @Override
+    @FXML
+    public void initialize() {
+        super.setGuiManager(GuiManager.getInstance());
+        this.clientManager = getGuiManager().getClientManager();
+    }
 
 
 }
